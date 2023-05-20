b3193b793cb5afc1303c6b73a2f20d70ae77fdc9
marat569 <marat569@gmail.com>
2023-05-20 04:22:59 -0400

    Add files via upload

---
+++ sk symlink local.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+set skDLL=C:\Program Files\Special K\SpecialK64.dll
+
+set gameDirectory=S:\Steam\steamapps\common\STAR OCEAN THE DIVINE FORCE
+
+set localAPI=dxgi.dll
+
+echo "making symlink"
+
+mklink "%gameDirectory%\%localAPI%" "%skDLL%"
+
+echo "making SpecialK.central"
+
+type NUL > "%gameDirectory%\SpecialK.central"
