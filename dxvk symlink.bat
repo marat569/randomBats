5704a610e99a8eebd925f09fcfd1ef69d41cfb5a
marat569 <marat569@gmail.com>
2023-05-20 04:45:50 -0400

    Add files via upload

---
+++ dxvk symlink.bat
@@ -0,0 +1,19 @@
+:: run as admin
+
+@ECHO OFF
+
+set gameDirectory=S:\Steam\steamapps\common\game
+
+set api=d3d9
+
+set apibit=32
+
+set dxvkDirectory=C:\Program Files\Special K\dxvk
+
+set dxvkDll=%dxvkDirectory%\x%apibit%\%api%.dll
+
+echo "making symlink"
+
+mklink "%gameDirectory%\%api%.dll" "%dxvkDll%"
+
+pause
